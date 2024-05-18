ff8515f41480c0812c08cc3ff3223d2960102629
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-05-18 15:07:48 -0500

    chore: initial commit

---
+++ lib/flakka_protoc_plugin.dart
@@ -0,0 +1,10 @@
+/// flakka_protoc_plugin, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate flakka_protoc_plugin
+/// dart pub global activate flakka_protoc_plugin
+///
+/// # see usage
+/// flakka_protoc_plugin --help
+/// ```
+library;
